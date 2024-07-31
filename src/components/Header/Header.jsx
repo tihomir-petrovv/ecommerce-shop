f3e918ff2cbef2bbc4ea7fec24ee77976f19ed8c
tihomir-petrovv <118721@students.ue-varna.bg>
2024-07-31 18:09:25 +0300

    Add cart item count to header
    The header component has been updated to display the number of items in the cart and favorites. This change improves the user experience by providing a visual indicator of the number of items in the cart and favorites.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,51 @@ import { NavLink, useNavigate } from "react-router-dom";
 import "./Header.css";
 import { PiShoppingCartThin } from "react-icons/pi";
 import { CiHeart } from "react-icons/ci";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../context/UserContext/UserContext";
 import { logoutUser } from "../../services/UserServices/auth-services";
+import {
+  getUserCart,
+  itemAmountLiveChange,
+} from "../../services/UserServices/cart-services";
+import {
+  favoriteItemsAmountLiveChange,
+  getUserFavoriteItems,
+} from "../../services/UserServices/favorite-services";
 
 export default function Header() {
   const { user, setUserContext } = useContext(AppContext);
   const navigate = useNavigate();
+  const [cartItems, setCartItems] = useState([]);
+  const [favoriteItems, setFavoriteItems] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      getUserCart(user.uid).then((data) => {
+        setCartItems(data);
+      });
+      getUserFavoriteItems(user.uid).then((data) => {
+        setFavoriteItems(data);
+      });
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (user) {
+      const unsubscribe = itemAmountLiveChange(user.uid, setCartItems);
+      return () => unsubscribe();
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (user) {
+      const unsubscribe = favoriteItemsAmountLiveChange(
+        user.uid,
+        setFavoriteItems
+      );
+      return () => unsubscribe();
+    }
+  }, [user]);
 
   const logOut = () => {
     logoutUser();
@@ -23,12 +61,18 @@ export default function Header() {
     <header id="header">
       <NavLink to={"/home"}>eShop.bg</NavLink>
       <input type="text" />
-      <NavLink to={"/cart"}>
-        <PiShoppingCartThin />
-      </NavLink>
-      <NavLink to={"/favorites"}>
-        <CiHeart />
-      </NavLink>
+      <div className="cart-items">
+        <NavLink to={"/cart"}>
+          <PiShoppingCartThin />
+          {cartItems && cartItems.length > 0 ? <h3>{cartItems.length}</h3> : null}
+        </NavLink>
+      </div>
+      <div className="cart-items">
+        <NavLink to={"/favorites"}>
+          <CiHeart />
+          {favoriteItems && favoriteItems.length > 0 ? <h3>{favoriteItems.length}</h3> : null}
+        </NavLink>
+      </div>
       {user ? (
         <div>
           <button onClick={logOut}>Log Out</button>
--- src/services/UserServices/favorite-services.js
+++ src/services/UserServices/favorite-services.js
@@ -1,4 +1,4 @@
-import { get, query, ref, set } from "firebase/database";
+import { get, off, onValue, query, ref, set } from "firebase/database";
 import { db } from "../../config/firebase-config";
 
 export const getUserFavoriteItems = async (userId) => {
@@ -28,4 +28,27 @@ export const setItemsToUserFavorites = (userId, product) => {
     amount: 1,
     addDate: new Date().valueOf(),
   });
+};
+
+export const favoriteItemsAmountLiveChange = (userId, setFavorites) => {
+  const cartRef = ref(db, `favorites/${userId}`);
+
+  const callback = (snapshot) => {
+    if (snapshot.exists()) {
+      const cart = Object.keys(snapshot.val()).map((key) => ({
+        id: key,
+        ...snapshot.val()[key],
+      }));
+
+      const sortedCart = cart.sort((a,b) => a.addDate - b.addDate)
+
+      setFavorites(sortedCart);
+    } else {
+      setFavorites(null);
+    }
+  };
+
+  onValue(cartRef, callback);
+
+  return () => off(cartRef, callback);
 };
